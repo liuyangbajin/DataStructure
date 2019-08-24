57b2c07c6a91d0ce4039d69364a1399a6aea9d05
18800102617 <liuyangbajin@163.com>
2019-08-24 18:20:47 +0800

    线性表顺序存储
    功能：
      -- 添加元素
      -- 删除元素
      -- 查询元素
    
    作用：
       分析线性表顺序存储的优缺点
    
    日期：
       2019-08-24

---
+++ src/com/bj/bs/linearlist/Model.java
@@ -0,0 +1,15 @@
+package com.bj.bs.linearlist;
+
+public class Model {
+
+    public int value;
+
+    public Model(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value+"";
+    }
+}
--- src/com/bj/bs/linearlist/T1.java
+++ src/com/bj/bs/linearlist/T1.java
@@ -4,5 +4,19 @@ public class T1 {
 
     public static void main(String[] args){
 
+        MyLinearList myLinearList = new MyLinearList(10);
+
+        // 指定位置插入元素
+        myLinearList.insertElementByIndex(new Model(3), 0);
+        myLinearList.insertElementByIndex(new Model(11),0);
+        myLinearList.insertElementByIndex(new Model(13),0);
+        myLinearList.insertElementByIndex(new Model(7),0);
+
+        // 删除元素
+        myLinearList.deleteElementByIndex(3);
+        myLinearList.showList();
+
+        // 获取线性表中的元素
+        System.out.println("获取的数组元素是：" + myLinearList.getElement(2));
     }
 }
